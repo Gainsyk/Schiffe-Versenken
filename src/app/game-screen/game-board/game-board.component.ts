f51f991cc56fc4d716c2310bddbcc9bb6424ae5f
Dmitri Volk <dmitri.volk@quinscape.de>
2025-03-03 13:09:00 +0100

    3

--- src/app/game-screen/game-board/game-board.component.ts
+++ src/app/game-screen/game-board/game-board.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {CellComponent} from './cell/cell.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-game-board',
-  imports: [CellComponent],
+  imports: [CellComponent, NgForOf],
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.css'
 })
 export class GameBoardComponent {
-
+cells = Array(100).fill(0);
 }
