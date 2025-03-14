5a68a42698a895138fa3c1b083ffa152d21ef37a
Dmitri Volk <dmitri.volk@quinscape.de>
2025-03-14 10:42:46 +0100

    6

--- src/app/game-screen/game-board/game-board.component.ts
+++ src/app/game-screen/game-board/game-board.component.ts
@@ -21,31 +21,32 @@ export class GameBoardComponent {
 
   onCellClick(cellIndex: number): void {
     // falls alle Schiffe platziert, abbruch
-    if (this.currentVesselIndex === this.vesselClasses.length) return;
+    if (this.areAllVesselsSet()) return;
 
     //Erstes Segment
-    if (this.currentVesselSections.length === 0) {
+    if (this.hasNoSegmentsYet()) {
       this.currentVesselSections.push(cellIndex);
       return;
     }
 
     //Jedes weitere Segment
-    if (this.currentVesselSections.length === 1) {
-      const firstIndex: number = this.currentVesselSections[0];
-      const {row: firstRow, col: firstCol} = this.getRowCol(firstIndex);
-      const {row: currentRow, col: currentCol} = this.getRowCol(cellIndex);
-
-      if ((Math.abs(firstRow - currentRow) === 1 && firstCol === currentCol) ||
-        (Math.abs(firstCol - currentCol) === 1 && firstRow === currentRow)) {
+    if (this.hasSegmentAlready()) {
+      if (this.isAdjacent(cellIndex)) {
         this.currentVesselSections.push(cellIndex);
-        this.orientation = (firstRow === currentRow) ? 'horizontal' : 'vertical';
       } else {
         alert('Schiffssegmente können nur anliegend in der Horizontalen oder Vertikalen angeordnet werden');
         return;
       }
     }
-    if (this.isAdjacent(cellIndex)) {
-      this.currentVesselSections.push(cellIndex);
+
+    this.resetVesselTracker();
+  }
+
+  private resetVesselTracker() {
+    if (this.currentVesselSections.length === this.vesselClasses[this.currentVesselIndex]) {
+      this.currentVesselSections = [];
+      this.currentVesselIndex++;
+      this.orientation = undefined;
     }
   }
 
@@ -57,25 +58,65 @@ export class GameBoardComponent {
   }
 
   isAdjacent(newIndex: number): boolean {
+    const lastIndex: number = this.currentVesselSections[this.currentVesselSections.length - 1];
     const firstIndex: number = this.currentVesselSections[0];
-    const currentIndex: number = this.currentVesselSections[this.currentVesselSections.length - 1];
 
     const {row: firstRow, col: firstCol} = this.getRowCol(firstIndex);
-    const {row: lastRow, col: lastCol} = this.getRowCol(currentIndex);
+    const {row: lastRow, col: lastCol} = this.getRowCol(lastIndex);
     const {row: newRow, col: newCol} = this.getRowCol(newIndex);
 
-    if (this.orientation === 'horizontal' && newRow === lastRow &&
-      (Math.abs(newCol - lastCol) === 1 || Math.abs(newCol - firstCol) === 1)) {
+    //Orientierung setzen beim zweiten Segment
+    if (this.isOnlyOneSegmentSet()) {
+      if (this.isSameCol(firstRow, newRow, firstCol, newCol) || this.isSameRow(firstCol, newCol, firstRow, newRow)) {
+        this.orientation = (firstRow === newRow) ? 'horizontal' : 'vertical';
+      }
+    }
+
+    //Zelle auf Anschluss prüfen
+    if (this.isValidHorizontalExtension(newRow, lastRow, newCol, lastCol, firstCol)) {
       return true;
     }
-    if (this.orientation === 'vertical' && newCol === lastCol &&
-      (Math.abs(newRow - lastRow) === 1 || Math.abs(newRow - firstRow) === 1)) {
+    if (this.isValidVerticalExtension(newCol, lastCol, newRow, lastRow, firstRow)) {
       return true;
     }
     return false;
   }
 
-  resetVesselPlacement(): void {
+  private hasSegmentAlready() {
+    return this.currentVesselSections.length > 0;
+  }
+
+  private hasNoSegmentsYet() {
+    return this.currentVesselSections.length === 0;
+  }
+
+  private areAllVesselsSet() {
+    return this.currentVesselIndex === this.vesselClasses.length;
+  }
+
+  private isOnlyOneSegmentSet() {
+    return this.currentVesselSections.length === 1;
+  }
+
+  private isSameRow(firstCol: number, newCol: number, firstRow: number, newRow: number) {
+    return Math.abs(firstCol - newCol) === 1 && firstRow === newRow;
+  }
+
+  private isSameCol(firstRow: number, newRow: number, firstCol: number, newCol: number) {
+    return Math.abs(firstRow - newRow) === 1 && firstCol === newCol;
+  }
+
+  private isValidVerticalExtension(newCol: number, lastCol: number, newRow: number, lastRow: number, firstRow: number) {
+    return this.orientation === 'vertical' && newCol === lastCol &&
+      (Math.abs(newRow - lastRow) === 1 || Math.abs(newRow - firstRow) === 1);
+  }
+
+  private isValidHorizontalExtension(newRow: number, lastRow: number, newCol: number, lastCol: number, firstCol: number) {
+    return this.orientation === 'horizontal' && newRow === lastRow &&
+      (Math.abs(newCol - lastCol) === 1 || Math.abs(newCol - firstCol) === 1);
+  }
+
+  resetGame(): void {
     this.currentVesselSections = [];
     this.orientation = undefined;
     for (let cell of this.cells) {
